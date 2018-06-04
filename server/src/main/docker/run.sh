58bbe50a3a055b5f76b1d318fcdaa1ad98563f41
Eugene Zadyra <untrance@gmail.com>
2018-06-04 23:47:38 +0200

    Use docker to run application

---
+++ server/src/main/docker/run.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+echo "********************************************************"
+echo "Waiting for the database server to start on port 5432"
+echo "********************************************************"
+while ! `nc -z database 5432`; do sleep 3; done
+echo "******** Database Server has started "
+
+echo "********************************************************"
+echo "Starting Luxmed Booking Service  "
+echo "********************************************************"
+cd /usr/local/luxmedbookingservice && java -jar @project.build.finalName@.jar
