ff7a098f7aedd4e975580794409e4fab31f461f4
Eugene Zadyra <untrance@gmail.com>
2018-12-05 18:08:46 +0100

    Updated docker configuration

--- docker/run.sh
+++ docker/run.sh
@@ -7,6 +7,26 @@ while ! `nc -z database 5432`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
+echo "Reading env file"
+echo "********************************************************"
+
+env="/lbs/config/env"
+
+if [ -f "$env" ]
+then
+  while IFS='=' read -r key value
+  do
+    key=$(echo ${key} | tr '.' '_')
+    export ${key}=${value}
+  done < "$env"
+
+else
+  echo "$env not found."
+  exit 1
+fi
+
+echo "********************************************************"
 echo "Starting Luxmed Booking Service  "
 echo "********************************************************"
-cd /usr/local/luxmedbookingservice && java -jar @project.build.finalName@.jar
+
+cd /app && java -jar server.jar
