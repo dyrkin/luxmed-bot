d259096208e4516da41055cce9f5977bf9bc0990
Eugene Zadyra <untrance@gmail.com>
2018-12-09 23:31:02 +0100

    Removed copyrights from all the files

--- api/src/main/scala/com/lbs/api/json/model/ValuationsRequest.scala
+++ api/src/main/scala/com/lbs/api/json/model/ValuationsRequest.scala
@@ -1,26 +1,4 @@
-/**
- * MIT License
- *
- * Copyright (c) 2018 Yevhen Zadyra
- *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
- *
- * The above copyright notice and this permission notice shall be included in all
- * copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
- * SOFTWARE.
- */
+
 package com.lbs.api.json.model
 
 import java.time.ZonedDateTime
@@ -61,5 +39,5 @@ import java.time.ZonedDateTime
 }
   */
 case class ValuationsRequest(clinicId: Long, doctorId: Long, payerDetailsList: List[PayerDetails],
-                                       referralRequiredByService: Boolean, roomId: Long, serviceId: Long,
-                                       startDateTime: ZonedDateTime) extends SerializableJsonObject
+                             referralRequiredByService: Boolean, roomId: Long, serviceId: Long,
+                             startDateTime: ZonedDateTime) extends SerializableJsonObject
--- server/src/main/scala/com/lbs/server/util/package.scala
+++ server/src/main/scala/com/lbs/server/util/package.scala
@@ -1,26 +1,4 @@
-/**
-  * MIT License
-  *
-  * Copyright (c) 2018 Yevhen Zadyra
-  *
-  * Permission is hereby granted, free of charge, to any person obtaining a copy
-  * of this software and associated documentation files (the "Software"), to deal
-  * in the Software without restriction, including without limitation the rights
-  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-  * copies of the Software, and to permit persons to whom the Software is
-  * furnished to do so, subject to the following conditions:
-  *
-  * The above copyright notice and this permission notice shall be included in all
-  * copies or substantial portions of the Software.
-  *
-  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-  * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-  * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-  * SOFTWARE.
-  */
+
 package com.lbs.server
 
 import java.time._
@@ -128,11 +106,11 @@ package object util {
   object MessageExtractors {
 
     object TextCommand {
-      def unapply(cmd: Command): Option[String] = if(cmd.callbackData.isEmpty) cmd.message.text.filter(_.nonEmpty) else None
+      def unapply(cmd: Command): Option[String] = if (cmd.callbackData.isEmpty) cmd.message.text.filter(_.nonEmpty) else None
     }
 
     object OptionalTextCommand {
-      def unapply(cmd: Command): Option[Option[String]] = if(cmd.callbackData.isEmpty) Some(TextCommand.unapply(cmd)) else None
+      def unapply(cmd: Command): Option[Option[String]] = if (cmd.callbackData.isEmpty) Some(TextCommand.unapply(cmd)) else None
     }
 
     object CallbackCommand {
