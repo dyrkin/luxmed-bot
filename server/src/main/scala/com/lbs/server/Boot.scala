df9a4f426779cee94fdcf67958f0c1651d804943
Eugene Zadyra <untrance@gmail.com>
2021-06-13 14:03:29 +0200

    #21 Use the latest openjdk docker image

--- server/src/main/scala/com/lbs/server/Boot.scala
+++ server/src/main/scala/com/lbs/server/Boot.scala
@@ -1,6 +1,7 @@
 
 package com.lbs.server
 
+import com.lbs.common.Logger
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
@@ -8,6 +9,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 @SpringBootApplication
 class Boot
 
-object Boot extends App {
+object Boot extends App with Logger {
+
+  def printInfo(): Unit = {
+    info(s"Num processors: ${Runtime.getRuntime.availableProcessors}")
+  }
+
+  printInfo()
   SpringApplication.run(classOf[Boot], args: _*)
 }
