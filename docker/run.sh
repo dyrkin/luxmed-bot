9c18675b8d26daf8c15faf877e385d3a664bd1dc
marska <github.m@skarupinscy.pl>
2022-01-13 20:25:42 +0100

    added possibility to change port

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port 5432"
+echo "Waiting for the database server to start on port ${DB_PORT:-5432}"
 echo "********************************************************"
-while ! `nc -z database 5432`; do sleep 3; done
+while ! `nc -z database ${DB_PORT:-5432}`; do sleep 3; done
 echo "******** Database Server has started "
 
 echo "********************************************************"
