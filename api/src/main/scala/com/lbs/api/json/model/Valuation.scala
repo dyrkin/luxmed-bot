36ada2874b64b3c99668024758d151b2d45b4beb
Eugene Zadyra <untrance@gmail.com>
2022-07-04 10:00:14 +0200

    productElementId can be null

--- api/src/main/scala/com/lbs/api/json/model/Valuation.scala
+++ api/src/main/scala/com/lbs/api/json/model/Valuation.scala
@@ -17,7 +17,7 @@ package com.lbs.api.json.model
  */
 
 case class Valuation(alternativePrice: Option[String], contractId: Long, isExternalReferralAllowed: Boolean,
-                     isReferralRequired: Boolean, payerId: Long, price: Double, productElementId: Long, productId: Long,
+                     isReferralRequired: Boolean, payerId: Long, price: Double, productElementId: Option[Long], productId: Long,
                      productInContractId: Long, requireReferralForPP: Boolean, valuationType: Long) {
 
 }
