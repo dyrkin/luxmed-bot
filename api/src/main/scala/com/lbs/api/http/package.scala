e0e87276d8a4091083f2741288097b046c1232db
materro <materro+github@outlook.it>
2023-12-22 14:27:31 +0100

    ApiHttp: Handle 401 Error (Unauthorized)

--- api/src/main/scala/com/lbs/api/http/package.scala
+++ api/src/main/scala/com/lbs/api/http/package.scala
@@ -74,6 +74,8 @@ package object http extends StrictLogging {
       code match {
         case HttpURLConnection.HTTP_MOVED_TEMP if httpResponse.header("Location").exists(_.contains("/LogOn")) =>
           Some(new SessionExpiredException)
+        case HttpURLConnection.HTTP_UNAUTHORIZED if lowercasedBody.contains("session has expired") =>
+          Some(new SessionExpiredException)
         case HttpURLConnection.HTTP_CONFLICT
             if lowercasedBody
               .contains("nieprawidłowy login lub hasło") || lowercasedBody.contains("invalid login or password") =>
