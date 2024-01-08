93eec8c46a8f26173b9988c83ba1aee09a175a22
Eugene Zadyra <untrance@gmail.com>
2024-01-08 16:01:53 +0100

    #58 resolve DB_HOST in the run.sh script

--- docker/run.sh
+++ docker/run.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 
 echo "********************************************************"
-echo "Waiting for the database server to start on port ${DB_PORT:-5432}"
+echo "Waiting for the database server to start on host ${DB_HOST:-localhost} and port ${DB_PORT:-5432}"
 echo "********************************************************"
-while ! `nc -z database ${DB_PORT:-5432}`; do sleep 3; done
+while ! `nc -z ${DB_HOST:-localhost} ${DB_PORT:-5432}`; do sleep 3; done
 echo "******** Database Server has been started "
 
 echo "********************************************************"
