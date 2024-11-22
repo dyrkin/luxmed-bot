0d990071adcbc95495ae3a1ab03622792dd5351e
Eugene Zadyra <untrance@gmail.com>
2024-11-22 01:41:07 +0100

    #80 fixed bug when the bot books an appointment but doesn't send a notification to a user and doesn't cancel monitoring

--- api/src/main/scala/com/lbs/api/json/model/ReservationConfirmResponse.scala
+++ api/src/main/scala/com/lbs/api/json/model/ReservationConfirmResponse.scala
@@ -7,8 +7,6 @@ package com.lbs.api.json.model
   *    "hasWarnings": false,
   *    "value": {
   *        "canSelfConfirm": false,
-  *        "eventType": 1,
-  *        "isTelemedicine": false,
   *        "npsToken": "babababa-9282-1662-a525-ababbabaa",
   *        "reservationId": 2222222,
   *        "serviceInstanceId": 33333333
@@ -26,8 +24,6 @@ case class ReservationConfirmResponse(
 
 case class ReservationConfirmValue(
   canSelfConfirm: Boolean,
-  eventType: Long,
-  isTelemedicine: Boolean,
   npsToken: String,
   reservationId: Long,
   serviceInstanceId: Long
